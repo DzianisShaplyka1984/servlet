b289fb30bb105998b6a2836b4ddb80a40079e140
Dzianis_Shaplyka <DShaplyka@inspirebrands.com>
2022-09-03 10:49:27 +0300

    Update Hello Servlet

--- src/main/java/com/academy/controller/HelloServlet.java
+++ src/main/java/com/academy/controller/HelloServlet.java
@@ -18,7 +18,7 @@ public class HelloServlet extends HttpServlet {
 
 
     writer.println("Hello world!");
-    writer.println("Hello world123!");
+    writer.println("Hello!");
 
     writer.println("</body>");
     writer.println("</html>");
